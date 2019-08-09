7f568d368b9906ec2ce0559c997bb210ad467475
MatthewASC6 <matthew.castillo2021@gmail.com>
2019-08-09 11:07:16 -0400

    New Database

--- popup.js
+++ popup.js
@@ -1,4 +1,9 @@
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+function copyUrl(){
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
     console.log(url);
-});
+
+})};
+
+copyUrl();
+
