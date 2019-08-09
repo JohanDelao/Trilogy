d13b16e109d3abbbc8d40c0d5873076747ba0fba
MatthewASC6 <matthew.castillo2021@gmail.com>
2019-08-09 13:55:46 -0400

    ALEX HELPS

--- popup.js
+++ popup.js
@@ -1,9 +1,7 @@
-function copyUrl(){
-    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+var database = firebase.database().ref();
+
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
+    database.push(url);
     console.log(url);
-
-})};
-
-copyUrl();
-
+});
