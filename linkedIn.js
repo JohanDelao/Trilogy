a33fc47bbb8e33f94847b892970e47ca7798b509
MatthewASC6 <matthew.castillo2021@gmail.com>
2019-08-12 13:17:58 -0400

    JSON

---
+++ linkedIn.js
@@ -0,0 +1,17 @@
+// var database = firebase.database().ref();
+
+// // Your web app's Firebase configuration
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyAOe-0gpbnSSpTvo1HGxtM8Yh-G1-nyfZU",
+//     authDomain: "trilogy-b291b.firebaseapp.com",
+//     databaseURL: "https://trilogy-b291b.firebaseio.com",
+//     projectId: "trilogy-b291b",
+//     storageBucket: "",
+//     messagingSenderId: "1041661731240",
+//     appId: "1:1041661731240:web:fa47e960c90ed822"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+
+document.querySelector("p").innerText="test";
+alert(123);
