02cc9e6f98d911e0eff85bed172bb47581828340
JohanDelaoASC6 <johandelao10@gmail.com>
2019-08-12 15:41:35 -0400

    manifest edits

--- popup.js
+++ popup.js
@@ -3,5 +3,10 @@ var database = firebase.database().ref();
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
     database.push(url);
-    console.log(url);
+    console.log(url)
+    let link = document.getElementById("inputLink")
+    link.value = url;
+
 });
+
+
