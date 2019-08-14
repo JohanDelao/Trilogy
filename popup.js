af75348ae10d02a1eb449f27da7e90b1773daa07
JohanDelaoASC6 <johandelao10@gmail.com>
2019-08-14 13:14:55 -0400

    Updated

--- popup.js
+++ popup.js
@@ -1,11 +1,21 @@
-var database = firebase.database().ref();
-
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
-    database.push(url);
-    console.log(url)
     let link = document.getElementById("inputLink")
     link.value = url;
 });
 
+
+$("a").click(function(){
+
+        /* Get the text field */
+        var copyText = document.getElementById("inputLink");
+      
+        /* Select the text field */
+        copyText.select();
+      
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
+
+
+})
 
